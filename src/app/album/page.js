ddb0520655dd59f37602a4fd64d41e0ad8084359
cherryctr <chrryctr1509@gmail.com>
2024-01-11 10:19:56 +0700

    link next

---
+++ src/app/album/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Album = () => {
+	return <h1>HALAMAN ALBUM</h1>;
+};
+
+export default Album;
---
+++ src/app/post/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Post = () => {
+	return <h1>HALAMAN POST</h1>;
+};
+
+export default Post;
